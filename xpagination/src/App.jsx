3b66d9fdedc9baf5e870b319412efc124b7414bf
Akshaykumar <akshaykumarpatil33@gmail.com>
2024-03-13 15:24:04 +0530

    final project commit

--- xpagination/src/App.jsx
+++ xpagination/src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [allDatas, setAllDatas] = useState([]);
+  const [pageDatas, setPageDatas] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [disabledPrev, setDisabledPrev] = useState(true);
+  const [disabledNext, setDisabledNext] = useState(false);
+
+  const itemsPerPage = 10;
+
+  useEffect(() => {
+    fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json')
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Failed to fetch employees');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setAllDatas(data);
+        updatePageData(data, currentPage);
+      })
+      .catch((err) => console.log("Error while fetching employees", err));
+  }, [currentPage]);
+
+  useEffect(() => {
+    setDisabledPrev(currentPage === 1);
+    setDisabledNext(currentPage === Math.ceil(allDatas.length / itemsPerPage));
+  }, [currentPage, allDatas.length]);
+
+  const updatePageData = (data, page) => {
+    const startIndex = (page - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    setPageDatas(data.slice(startIndex, endIndex));
+  };
+
+  const handlePrevious = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const handleNext = () => {
+    setCurrentPage(currentPage + 1);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h1>Employee Data Table</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pageDatas.map((d) => (
+              <tr key={d.id}>
+                <td>{d.id}</td>
+                <td>{d.name}</td>
+                <td>{d.email}</td>
+                <td>{d.role}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className='pagination'>
+          <button onClick={handlePrevious} disabled={disabledPrev}>Previous</button>
+          <p>{currentPage}</p>
+          <button onClick={handleNext} disabled={disabledNext}>Next</button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
